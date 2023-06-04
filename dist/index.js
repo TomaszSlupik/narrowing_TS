650151c4edf290be13da8975d147d75b4aa316de
TomaszSlupik <slupiktomasz@gmail.com>
2023-06-04 19:50:08 +0200

    change

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,26 @@
 "use strict";
-console.log('Narrowing');
+// Type of
+// 1 przykład  
+const numberOrString = (el) => {
+    if (typeof el === "string") {
+        console.log('To jest string');
+    }
+    if (typeof el === "number") {
+        console.log('To jest number');
+    }
+};
+numberOrString(10);
+// 2 przykład 
+const stringOrNull = (el) => {
+    if (!el) {
+        console.log('To jest null');
+    }
+    else if (typeof el === 'string') {
+        console.log(el);
+    }
+    else if (Array.isArray(el)) {
+        el.forEach((element) => {
+            console.log(element);
+        });
+    }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('Narrowing')
+// Type of
+
+// 1 przykład  
+const numberOrString = (el: string | number) => {
+    if (typeof el === "string") {
+        console.log('To jest string')
+    }
+    if (typeof el === "number") {
+        console.log('To jest number')
+    }
+}
+
+numberOrString(10)
+
+// 2 przykład 
+const stringOrNull = (el: string | null) => {
+    if (!el) {
+        console.log('To jest null');
+    } else if (typeof el === 'string') {
+        console.log(el);
+    } else if (Array.isArray(el)) {
+        el.forEach((element: string) => {
+            console.log(element);
+        });
+    }
+};
